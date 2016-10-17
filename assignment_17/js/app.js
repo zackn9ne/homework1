017421ba6c7ccf5c596b0946bb3a3d415375dbb3
Rudy <Rudy@Rudys-MacBook-Pro.local>
2016-10-17 10:09:43 -0400

    assignment 17

---
+++ assignment_17/js/app.js
@@ -0,0 +1,15 @@
+console.log( "app.js was just hit" );
+var weeksInYear = 52
+var weeksElapsed = 41
+var remainingWeeks = weeksInYear - weeksElapsed + " more weeks "
+document.write (remainingWeeks);
+
+var greet = "hello "
+var who = "class "
+var greeting = greet + who
+document.write (greeting);
+
+var one = 1
+var two = 2
+var three = one + two
+// document.write (<p>three</p>); "why does this break my code??? how would I use <p> or <div> here?"
